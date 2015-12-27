e6a1103008eb12ff438cc8b87efc2ba0eade44d0
Jason C Miller <cyzicusgit@gmail.com>
2015-12-26 18:47:52 -0700

    inner beans

---
+++ lesson-017/src/main/java/us/cyzic/tutorial/udemy/spring/framework/Animal.java
@@ -0,0 +1,37 @@
+package us.cyzic.tutorial.udemy.spring.framework;
+
+public class Animal {
+	
+	private String name;
+	private String type;
+	
+	public Animal(String name, String type) {
+		super();
+		this.name = name;
+		this.type = type;
+	}
+	
+	public Animal() {
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	@Override
+	public String toString() {
+		return "Animal [name=" + name + ", type=" + type + "]";
+	}
+	
+	
+
+}
