333d285b06a8a4f0d18c67b4b8e01169fa670fde
Jason C Miller <cyzicusgit@gmail.com>
2015-12-27 14:17:24 -0700

    autowiring by type

---
+++ lesson-020/src/main/java/us/cyzic/tutorial/udemy/spring/framework/App.java
@@ -0,0 +1,20 @@
+package us.cyzic.tutorial.udemy.spring.framework;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("us/cyzic/tutorial/udemy/spring/framework/beans/beans.xml");
+		
+		Logger logger = (Logger)context.getBean("logger");
+		
+		logger.writeConsole("Should be displayed on cosole");
+		logger.writeFile("I want this line of text in a file");
+		
+		context.close();
+	}
+
+}
---
+++ lesson-020/src/main/java/us/cyzic/tutorial/udemy/spring/framework/LogWriter.java
@@ -0,0 +1,7 @@
+package us.cyzic.tutorial.udemy.spring.framework;
+
+public interface LogWriter {
+	
+	public void write(String text);
+
+}
