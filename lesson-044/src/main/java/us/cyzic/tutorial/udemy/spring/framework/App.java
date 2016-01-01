ea2014d14d4bfe8bfdbd75d66307629878ccaee2
Jason C Miller <cyzicusgit@gmail.com>
2016-01-01 13:20:20 -0700

    spring database exceptions

---
+++ lesson-044/src/main/java/us/cyzic/tutorial/udemy/spring/framework/App.java
@@ -0,0 +1,37 @@
+package us.cyzic.tutorial.udemy.spring.framework;
+
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"us/cyzic/tutorial/udemy/spring/framework/beans/beans.xml");
+
+		OffersDAO offersDao = (OffersDAO) context.getBean("offersDao");
+		
+		try {
+			List<Offer> offers = offersDao.getOffers();
+
+			for (Offer offer : offers) {
+				System.out.println(offer.toString());
+			}
+		} catch (CannotGetJdbcConnectionException ex) {
+			System.out.println("Cannot connect to database");
+		} catch (DataAccessException dae) {
+			System.out.println(dae.getMessage());
+			System.out.println(dae.getClass());
+		} catch (Exception ex) {
+			System.out.println("Exception caught: " + ex.getMessage());
+			System.out.println("Exception type: " + ex.getClass());
+		}
+
+		context.close();
+	}
+
+}
---
+++ lesson-044/src/main/java/us/cyzic/tutorial/udemy/spring/framework/OffersDAO.java
@@ -0,0 +1,42 @@
+package us.cyzic.tutorial.udemy.spring.framework;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+@Component("offersDao")
+public class OffersDAO {
+	
+	private JdbcTemplate jdbc;
+	
+	@Autowired
+	private void setDataSource(DataSource dataSource) {
+		this.jdbc = new JdbcTemplate(dataSource);
+	}
+	
+	public List<Offer> getOffers() {
+		
+		return jdbc.query("select * from offers", new RowMapper<Offer>() {
+
+			public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return new Offer(
+						rs.getInt("id"), 
+						rs.getString("name"), 
+						rs.getString("email"), 
+						rs.getString("text")
+				);
+				
+			}
+			
+		});
+		
+	}
+
+}
